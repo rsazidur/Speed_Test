08464485c2233a2da025d536ae27004e992b14f5
rsazidur <rsazidur936@gmail.com>
2023-05-25 23:41:13 +0600

    Done

--- Speed_Test.py
+++ Speed_Test.py
@@ -1,25 +1,37 @@
 from tkinter import *
 import speedtest
 
+
+def speedcheck():
+    sp = speedtest.Speedtest()
+    sp.get_servers()
+    down = str(round(sp.download() / (10 ** 6), 3)) + " Mbps"
+    up = str(round(sp.download() / (10 ** 6), 3)) + " Mbps"
+    down_label.config(text=down)
+    up_label.config(text=up)
+
+
 sp = Tk()
 sp.title(" Internet Speed Test")
-sp.geometry("500x500")
+sp.geometry("500x700")
 sp.config(bg="White")
 
-label = Label(sp, text="Internet Speed Test", font=("serif", 20))
+label = Label(sp, text="Internet Speed Test", font=("serif", 20, "bold"))
 label.place(x=60, y=40, height=50, width=380)
 
 label = Label(sp, text="Download Speed", font=("serif", 20))
 label.place(x=60, y=130, height=50, width=380)
 
-label = Label(sp, text="00", font=("serif", 20))
+down_label = Label(sp, text="00", font=("serif", 20))
 label.place(x=60, y=200, height=50, width=380)
 
 label = Label(sp, text="Upload Speed", font=("serif", 20))
 label.place(x=60, y=290, height=50, width=380)
 
-label = Label(sp, text="00", font=("serif", 20))
+up_label = Label(sp, text="00", font=("serif", 20))
 label.place(x=60, y=360, height=50, width=380)
 
+button = Button(sp, text="Check Speed", font=("serif", 20), relief=RAISED, bg="Gray", command=speedcheck)
+button.place(x=60, y=460, height=50, width=380)
 
 sp.mainloop()
