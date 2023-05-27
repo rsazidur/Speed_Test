2670214f39e79d7e0fb8be3300c1af8bee146124
rsazidur <rsazidur936@gmail.com>
2023-05-27 07:59:39 +0600

    Project Complete

--- Speed_Test.py
+++ Speed_Test.py
@@ -16,22 +16,22 @@ sp.title(" Internet Speed Test")
 sp.geometry("500x700")
 sp.config(bg="White")
 
-label = Label(sp, text="Internet Speed Test", font=("serif", 20, "bold"))
+label = Label(sp, text="Internet Speed Test", font=("Time New Roman", 20, "bold"))
 label.place(x=60, y=40, height=50, width=380)
 
-label = Label(sp, text="Download Speed", font=("serif", 20))
+label = Label(sp, text="Download Speed", font=("Time New Roman", 20))
 label.place(x=60, y=130, height=50, width=380)
 
-down_label = Label(sp, text="00", font=("serif", 20))
-label.place(x=60, y=200, height=50, width=380)
+down_label = Label(sp, text="00", font=("Time New Roman", 20), bg="Gray", fg="White")
+down_label.place(x=60, y=200, height=50, width=380)
 
-label = Label(sp, text="Upload Speed", font=("serif", 20))
+label = Label(sp, text="Upload Speed", font=("Time New Roman", 20))
 label.place(x=60, y=290, height=50, width=380)
 
-up_label = Label(sp, text="00", font=("serif", 20))
-label.place(x=60, y=360, height=50, width=380)
+up_label = Label(sp, text="00", font=("Time New Roman", 20), bg="Gray", fg="White")
+up_label.place(x=60, y=360, height=50, width=380)
 
-button = Button(sp, text="Check Speed", font=("serif", 20), relief=RAISED, bg="Gray", command=speedcheck)
+button = Button(sp, text="Check Speed", font=("Time New Roman", 20), relief=RAISED, bg="Gray", command=speedcheck)
 button.place(x=60, y=460, height=50, width=380)
 
 sp.mainloop()
